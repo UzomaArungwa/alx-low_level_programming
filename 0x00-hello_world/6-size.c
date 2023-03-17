c0dd9ca4dbf2a2cf36b36149bf58ee94a23dbac7
UzomaArungwa <arungwauzoma@gmail.com>
2023-03-16 20:35:26 -0700

    qu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int c;
 long long int d;
 float f;
 
-printf("size of a char: %lu byte (s)\n", (unsigned long) sizeof(a));
-printf("size of a int:  %lu byte (s)\n", (unsigned long)sizeof(b));
-printf("size of a long int:  %lu byte (s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int:  %lu byte (s)\n", (unsigned long)sizeof(d));
-printf("size of a float:  %lu byte (s)\n", (unsigned long)sizeof(f));
+printf("size of a char: %d byte (s)\n", (unsigned long)Sizeof(a));
+printf("size of a int:  %d byte (s)\n", (unsigned long)Sizeof(b));
+printf("size of a long int:  %d byte (s)\n", (unsigned long)Sizeof(c));
+printf("size of a long long int:  %d byte (s)\n", (unsigned long)Sizeof(d));
+printf("size of a float:  %d byte (s)\n", (unsigned long)Sizeof(f));
 return (0);
 }
